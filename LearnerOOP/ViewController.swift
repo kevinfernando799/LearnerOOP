7874ae808f64421be0e196a799366680449ebc2a
kevin fernando <kevinfernando799@gmail.com>
2019-05-15 15:44:59 +0700

    add  ew Facilitator Model and UIButton

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -17,18 +17,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var learnerGenderLabel: UILabel!
     
     var learnerInstance: LearnerModel?
+    var facilitatorInstance: FacilitatorModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         learnerInstance = LearnerModel (nameLearner: "Kevin", ageLearner: 20, genderLearner: "male", imageProfileLearner: "<#T##String#>")
-        
+        facilitatorInstance = FacilitatorModel(facilName: "kevin", facilAge: 20, facilGender: "male", facilImageProfile: "", facilperk: "free burger ")
         updateUI()
         
     }
     
     @IBAction func increaseAgeButton(_ sender: UIButton) {
-        if let instance = learnerInstance {
+        if let instance = facilitatorInstance {
             instance.increaseAge()
+            instance.coachMentee()
             updateUI()
         }
         
@@ -36,7 +38,7 @@ class ViewController: UIViewController {
     
     
     func updateUI () {
-        if let instance = learnerInstance {
+        if let instance = facilitatorInstance {
             learnerNameLabel.text = instance.name
             learnerAgeLabel.text = "\(instance.age)"
             learnerGenderLabel.text = instance.gender
